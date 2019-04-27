429912fca453fd8e99c2f0ac135c0a31645af06f
woody <woody_me@qq.com>
2019-04-27 22:50:31 +0800

    add echo server

---
+++ shadowsocks-netty-server-echo/src/main/java/org/shadowsocks/netty/server/EchoServer.java
@@ -0,0 +1,62 @@
+package org.shadowsocks.netty.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EchoServer {
+
+	private static Logger logger = LoggerFactory.getLogger(EchoServer.class);
+
+	//private static final String CONFIG = "conf/config.xml";
+
+	private EventLoopGroup bossGroup = null;
+	private EventLoopGroup workerGroup = null;
+	private ServerBootstrap bootstrap = null;
+	private static EchoServer echoServer = new EchoServer();
+
+	public static EchoServer getInstance() {
+		return echoServer;
+	}
+
+
+
+    public static void main(String[] args) {
+        EchoServer.getInstance().start();
+    }
+
+
+    public void start() {
+		try {
+			int port = 10802;
+			bossGroup = new NioEventLoopGroup(1);
+			workerGroup = new NioEventLoopGroup();
+			bootstrap = new ServerBootstrap();
+			bootstrap.group(bossGroup, workerGroup)
+					.channel(NioServerSocketChannel.class)
+					.childOption(ChannelOption.SO_KEEPALIVE, true)
+					.childHandler(new EchoHandler());
+			logger.info("Echo Start At Port {} " ,port);
+			bootstrap.bind(port).sync().channel().closeFuture().sync();
+		} catch (Exception e) {
+			logger.error("start error", e);
+		} finally {
+			stop();
+		}
+	}
+
+	public void stop() {
+		if (bossGroup != null) {
+			bossGroup.shutdownGracefully();
+		}
+		if (workerGroup != null) {
+			workerGroup.shutdownGracefully();
+		}
+		logger.info("Stop Server!");
+	}
+
+}
